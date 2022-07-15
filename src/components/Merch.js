34a351a87483a30deaa9024e2e8080cd5fcbf3a7
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-07-14 23:50:31 -0400

    dropped mui made skelleton cards and started youtube API

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,9 +1,6 @@
 
 const Merch = () => {
 
-
-
-
     return (
         <>
         
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,117 +1,18 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Skeleton from '@mui/material/Skeleton';
-import Avatar from '@mui/material/Avatar';
-import Icon from '../images/bigMoodIcon.jpeg'
-const data = [
-    {
-        src: 'https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA',
-        title: 'Big Mood - Might Be Me',
-        channel: 'Big Mood',
-        views: '1.5 k views',
-        createdAt: '3 years ago',
-    },
-    {
-        src: 'https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA',
-        title: 'Big Mood - Vertigo',
-        channel: 'Big Mood',
-        views: '1.1 k views',
-        createdAt: '3 years ago',
-    },
-    {
-        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
-        title: 'Big Mood - Slippin',
-        channel: 'Big Mood',
-        views: '687 views',
-        createdAt: '3 years ago',
-    },
-    {
-        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
-        title: 'Big Mood - Let Go',
-        channel: 'Big Mood',
-        views: '682 views',
-        createdAt: '3 years ago',
-    },
-    {
-        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
-        title: 'Big Mood - Let Go',
-        channel: 'Big Mood',
-        views: '426 views',
-        createdAt: '2 years ago',
-    },
-    {
-        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
-        title: 'Big Mood - Ricochet',
-        channel: 'Big Mood',
-        views: '322 views',
-        createdAt: '2 years ago',
-    },
-];
 
-function Videos(props) {
-  const { loading = false } = props;
+const Videos = () => {
+    const channel_ID=''
+    const youTubeLink = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&channelId=${channel_ID}&part=snippet,id&order=date&maxResults=20`
+    // After that you will receive a JSON with video ids and details, and you can construct your video URL like this:
+    // const video_ID = ''
+    // const videoURL = `http://www.youtube.com/watch?v=${video_ID}`
 
-  return (
-    <Grid container wrap="wrap">
-      {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
-        <Box key={index} sx={{ width: 250, marginRight: 0.5, my: 5 }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box sx={{ margin: 1 }}>
-          {loading ? (
-            <Skeleton variant="circular">
-              <Avatar />
-            </Skeleton>
-          ) : (
-            <Avatar src={Icon} />
-          )}
-        </Box>
-        <Box sx={{ width: '100%' }}>
-          {loading ? (
-            <Skeleton width="100%">
-              <Typography>.</Typography>
-            </Skeleton>
-          ) : (
-            <Typography>Big Mood</Typography>
-          )}
-        </Box>
-      </Box>
-          {item ? (
-            <img
-              style={{ width: 250, height: 158 }}
-              alt={item.title}
-              src={item.src}
-            />
-          ) : (
-            <Skeleton variant="rectangular" width={250} height={158} />
-          )}
-
-          {item ? (
-            <Box sx={{ pr: 2 }}>
-              <Typography gutterBottom variant="body2">
-                {item.title}
-              </Typography>
-              <Typography display="block" variant="caption" color="black">
-                {item.channel}
-              </Typography>
-              <Typography variant="caption" color="black">
-                {`${item.views} • ${item.createdAt}`}
-              </Typography>
-            </Box>
-          ) : (
-            <Box sx={{ pt: 0.5 }}>
-              <Skeleton />
-              <Skeleton width="60%" />
-            </Box>
-          )}
-        </Box>
-      ))}
-    </Grid>
-  );
+    return (
+        <>
+        
+        <h1>Videos Page</h1>
+        
+        </>
+    )
 }
 
 export default Videos
-
-
