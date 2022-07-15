205e241481ae422cb8b2e2b743e234290610046b
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-07-14 23:02:29 -0400

    videos page started, will most likely scrap for api

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -4,7 +4,8 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Skeleton from '@mui/material/Skeleton';
-
+import Avatar from '@mui/material/Avatar';
+import Icon from '../images/bigMoodIcon.jpeg'
 const data = [
     {
         src: 'https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA',
@@ -51,54 +52,66 @@ const data = [
 ];
 
 function Videos(props) {
-    const { loading = false } = props;
+  const { loading = false } = props;
 
-    return (
-        <Grid container wrap="wrap">
-            {(loading ? Array.from(new Array(5)) : data).map((item, index) => (
-                <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
-                {item ? (
-                    <img
-                    style={{ width: 210, height: 118 }}
-                    alt={item.title}
-                    src={item.src}
-                    />
-                ) : (
-                    <Skeleton variant="rectangular" width={210} height={118} />
-                )}
-        
-                {item ? (
-                    <Box sx={{ pr: 2 }}>
-                    <Typography gutterBottom variant="body2">
-                        {item.title}
-                    </Typography>
-                    <Typography display="block" variant="caption" color="black">
-                        {item.channel}
-                    </Typography>
-                    <Typography variant="caption" color="black">
-                        {`${item.views} • ${item.createdAt}`}
-                    </Typography>
-                    </Box>
-                ) : (
-                    <Box sx={{ pt: 0.5 }}>
-                    <Skeleton />
-                    <Skeleton width="60%" />
-                    </Box>
-                )}
-                </Box>
-            ))}
-            </Grid>
-        );
-    }
-    Videos.propTypes = {
-    loading: PropTypes.bool,
-    };
-
-    export default function YouTube() {
-    return (
-        <Box sx={{ overflow: 'hidden' }}>
-        <Videos loading />
-        <Videos />
+  return (
+    <Grid container wrap="wrap">
+      {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+        <Box key={index} sx={{ width: 250, marginRight: 0.5, my: 5 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ margin: 1 }}>
+          {loading ? (
+            <Skeleton variant="circular">
+              <Avatar />
+            </Skeleton>
+          ) : (
+            <Avatar src={Icon} />
+          )}
         </Box>
-    );
+        <Box sx={{ width: '100%' }}>
+          {loading ? (
+            <Skeleton width="100%">
+              <Typography>.</Typography>
+            </Skeleton>
+          ) : (
+            <Typography>Big Mood</Typography>
+          )}
+        </Box>
+      </Box>
+          {item ? (
+            <img
+              style={{ width: 250, height: 158 }}
+              alt={item.title}
+              src={item.src}
+            />
+          ) : (
+            <Skeleton variant="rectangular" width={250} height={158} />
+          )}
+
+          {item ? (
+            <Box sx={{ pr: 2 }}>
+              <Typography gutterBottom variant="body2">
+                {item.title}
+              </Typography>
+              <Typography display="block" variant="caption" color="black">
+                {item.channel}
+              </Typography>
+              <Typography variant="caption" color="black">
+                {`${item.views} • ${item.createdAt}`}
+              </Typography>
+            </Box>
+          ) : (
+            <Box sx={{ pt: 0.5 }}>
+              <Skeleton />
+              <Skeleton width="60%" />
+            </Box>
+          )}
+        </Box>
+      ))}
+    </Grid>
+  );
 }
+
+export default Videos
+
+
