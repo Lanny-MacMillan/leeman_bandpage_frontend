6c0a03b287c6b4e501300863d9ac90c452b553f3
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-07-31 22:42:30 -0400

    fixed mobile about me, included photos

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,42 +1,42 @@
 import '../App.css';
-// import {useState, useEffect} from 'react'
-// import { storage } from "./firebase";
-// import { ref, uploadBytes, listAll, getDownloadURL, list } from "firebase/storage"
-// import { v4 } from 'uuid'
+import {useState, useEffect} from 'react'
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL, list } from "firebase/storage"
+import { v4 } from 'uuid'
 
 const Images = (props) => {
-    // const [imageUpload, setImageUpload] = useState(null)
-    // const [imageUrls, setImageUrls] = useState([]);
+    const [imageUpload, setImageUpload] = useState(null)
+    const [imageUrls, setImageUrls] = useState([]);
 
-    // const imagesListRef = ref(storage, "images/")
+    const imagesListRef = ref(storage, "images/")
 
-    // const uploadImage = () => {
-    //     if (imageUpload == null) {
-    //         alert('No Image Selected')
-    //         return;
-    //     }
-    //     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`)
-    //     uploadBytes(imageRef, imageUpload).then((snapshot) => {
-    //         getDownloadURL(snapshot.ref).then((url) => {
-    //             setImageUrls((prev) => [...prev, url])
-    //         })
-    //         alert('Image uploaded')
-    //     })
-    // }
+    const uploadImage = () => {
+        if (imageUpload == null) {
+            alert('No Image Selected')
+            return;
+        }
+        const imageRef = ref(storage, `images/${imageUpload.name + v4()}`)
+        uploadBytes(imageRef, imageUpload).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                setImageUrls((prev) => [...prev, url])
+            })
+            alert('Image uploaded')
+        })
+    }
 
-    // const getImages = () => {
+    const getImages = () => {
 
-    // }
+    }
 
-    // useEffect(() => {
-    //     listAll(imagesListRef).then((response) => {
-    //         response.items.forEach((item) => {
-    //             getDownloadURL(item).then((url) => {
-    //             setImageUrls((prev) => [...prev, url]);
-    //             });
-    //         });
-    //     });
-    // }, []);
+    useEffect(() => {
+        listAll(imagesListRef).then((response) => {
+            response.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                setImageUrls((prev) => [...prev, url]);
+                });
+            });
+        });
+    }, []);
     return (
         <> 
         <div id='merchBackground' 
@@ -46,7 +46,7 @@ const Images = (props) => {
             <div className="showContainer">
                 <h1 id='imagesH1'>Images Coming Soon</h1>
             </div>
-            {/* <div className='imageFile'>
+            <div className='imageFile'>
                 <input type='file' onChange={(event) => {setImageUpload(event.target.files[0]);
                 }}/>
                 <br/>
@@ -57,7 +57,7 @@ const Images = (props) => {
                     return <img className='urlImg' src={url}
                 />
             })}
-            </div>   */}
+            </div>  
         </div>
         </>
     )
